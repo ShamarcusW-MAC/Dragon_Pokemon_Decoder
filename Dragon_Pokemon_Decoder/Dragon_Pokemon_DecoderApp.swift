01e5efb14a1e1b95e7d6b9514b345caccc48c8c0
ShaMarcusW-MAC <shamarcus.walker@mobileappscompany.us>
2023-01-24 10:12:48 -0500

    Initial Commit

---
+++ Dragon_Pokemon_Decoder/Dragon_Pokemon_DecoderApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dragon_Pokemon_DecoderApp.swift
+//  Dragon_Pokemon_Decoder
+//
+//  Created by Consultant on 1/24/23.
+//
+
+import SwiftUI
+
+@main
+struct Dragon_Pokemon_DecoderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
